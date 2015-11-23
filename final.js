1c9f2206e72b8a6747603d3fe70ceeffb4e5ba6d
Jacques Wyatt Therrien <jake.therrien13@gmail.com>
2015-11-22 18:35:29 -0800

    added final code

---
+++ final.js
@@ -0,0 +1,25 @@
+// Create a generic function to log the response from Mandrill API
+function log(obj) {
+    $('#response').text(JSON.stringify(obj));
+}
+
+// Create a new instance of the Mandrill class from the mandrill
+// library. It takes one parameter, your API key.
+
+var m = new mandrill.Mandrill('Fi1hgj6aNP06OJfjfPQZ2Q');
+
+// create a variable for the API call
+var content = {
+	"message": {
+		"from_email":"jake.therrien13@gmail.com",
+		"to":[{"email":"jake.therrien13@gmail.com"}],
+		"subject":"Mandrill Test",
+		"text":"This is my first time using Mandrill!"
+	}
+};
+
+// send the email
+function sendTheMail() {
+	m.messages.send(content);
+	console.log("email sent successfully!");
+};
